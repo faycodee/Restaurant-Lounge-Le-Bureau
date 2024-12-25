bc979d677107da689ed9b4696583dc44a4b86f77
Faycal <faysaloumzil1@gmail.com>
2024-12-25 17:42:05 +0100

    Faycal: try to rebuild Header

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -48,18 +48,18 @@ const Header = () => (
   //   </section>
   // </div>
 
-  // <div className="bg-primary-black app__wrapper section__padding font-CormorantUpright text-primary-golden" id="home">
-  //   <div className="flex-1 w-full flex items-start justify-center flex-col">
-  //     <SubHeading title="Chase the new flavour" />
-  //     <h1 className="headtext__cormorant font-bold mb-4">The Key To Fine Dining</h1>
-  //     <p className="p__opensans mb-4">Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet tellus </p>
-  //     <button type="button" className="custom__button">Explore Menu</button>
-  //   </div>
+  <div className="bg-primary-black app__wrapper section__padding font-CormorantUpright text-primary-golden" id="home">
+    <div className="flex-1 w-full flex items-start justify-center flex-col">
+      <SubHeading title="Chase the new flavour" />
+      <h1 className="headtext__cormorant font-bold mb-4">The Key To Fine Dining</h1>
+      <p className="p__opensans mb-4">Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet tellus </p>
+      <button type="button" className="custom__button">Explore Menu</button>
+    </div>
 
-  //   <div className="app__wrapper_img">
-  //     <img src={images.welcome} alt="header_img" />
-  //   </div>
-  // </div>
+    <div className="app__wrapper_img">
+      <img src={images.welcome} alt="header_img" />
+    </div>
+  </div>
 );
 
 export default Header;
