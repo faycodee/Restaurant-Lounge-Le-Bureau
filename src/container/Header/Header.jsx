c9d5ab860ae9998062a56b0b1c3b51777714545c
Faycal <faysaloumzil1@gmail.com>
2024-12-27 23:49:13 +0100

    Faycal: add styls layers tailwindcss

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -6,7 +6,7 @@ import "./Header.css";
 
 const Header = () => (
  
-  <div className="bg-black app__wrapper section__padding font-CormorantUpright text-primary-golden" id="home">
+  <div className="bg-black app__wrapper overflow-x-hidden h-dvh section__padding font-CormorantUpright text-primary-golden" id="home">
     
     {/* <div className="flex-1 w-full flex items-start justify-center flex-col">
       <SubHeading title="Chase the new flavour" />
