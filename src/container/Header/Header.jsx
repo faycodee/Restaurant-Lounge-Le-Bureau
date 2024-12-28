7d78eb89d0d9c2adea357d8d0abc17be19ceac22
Faycal <faysaloumzil1@gmail.com>
2024-12-28 01:32:49 +0100

    Faycal:add videos for background and make header better

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -12,41 +12,68 @@ const Header = () => {
   const [loadedVideos, setLoadedVideos] = useState(0);
   const totalVideos = 4;
   const nextVidRef = useRef(null);
-  const getSrc = (i) => `../public/videos/${i}.mp4`;
+  const nextIndex = (curIndex % totalVideos) + 1;
+  const getSrc = (i) => `./videos/${i}.mp4`;
   const handleMiniVdClick = () => {
     setHasClicked(true);
-    setCurIndex((preInd) => preInd + 1);
+    setCurIndex(nextIndex);
   };
   const handelVidLoad = () => {
     setLoadedVideos((prev) => prev + 1);
   };
   return (
     <div
-      className="relative h-dvh w-screen overflow-x-hidden bg-black 
+      className="relative w-[100vw] h-dvh  overflow-x-hidden bg-black 
       app__wrapper section__padding font-CormorantUpright text-primary-golden"
       id="home"
     >
       <div
         id="video-frame"
         className="relative z-10 h-dvh w-screen overflow-hidden rounded-xl "
-      ></div>
-      <div className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-auto rounded-lg">
-        <div
-          onClick={handleMiniVdClick}
-          className="origin-center  scale-50 opacity-0 
-        transition-all duration-500 ease-in hover:scale-100 hover:opacity-100 "
-        >
-          <video
-            ref={nextVidRef}
-            src={getSrc(curIndex +1)}
-            loop
-            muted
-            id="current-video"
-            className="size-64 origin-center scale-150 object-cover object-center overflow-hidden"
-            onLoadedData={handelVidLoad}
-          ></video>
+      >
+        {" "}
+        <div className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-auto rounded-lg">
+          <div
+            onClick={handleMiniVdClick}
+            className="origin-center   scale-50 opacity-0  overflow-hidden
+        transition-all duration-500  ease-in hover:scale-100 hover:opacity-100 "
+          >
+            <video
+              ref={nextVidRef}
+              src={getSrc(nextIndex)}
+              loop
+              muted
+              id="current-video"
+              className="size-64 origin-center scale-150 object-cover object-center "
+              onLoadedData={handelVidLoad}
+            ></video>
+          </div>
         </div>
+        <video 
+       ref={nextVidRef}
+       src={getSrc(curIndex)}
+       loop
+       muted
+       id="next-video"
+       className=" absolute absolute-center invisible size-64  object-cover object-center "
+       onLoadedData={handelVidLoad}
+        >
+          
+        </video>
+        <video 
+        ref={nextVidRef}
+        src={getSrc(curIndex==totalVideos-1 ? 1:curIndex)}
+        loop
+        autoPlay
+        muted
+        className=" absolute left-0 top-0  size-full  object-cover object-center "
+
+        id="next-video"
+        >
+
+        </video>
       </div>
+
       {/* <div className="flex-1 w-full flex items-start justify-center flex-col">
       <SubHeading title="Chase the new flavour" />
       <h1 className="headtext__cormorant font-bold mb-4">The Key To Fine Dining</h1>
