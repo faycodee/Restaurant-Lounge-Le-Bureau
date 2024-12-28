58567a02050467bb0af3f273105a6beb35c6cabf
Faycal <faysaloumzil1@gmail.com>
2024-12-28 23:23:45 +0100

    Faycal: add  headerTextsjs.js

--- src/Slices/headerTexts.js
+++ src/Slices/headerTexts.js
@@ -1,10 +1,17 @@
-import { createSlice } from "@reduxjs/toolkit";
+// import { createSlice } from "@reduxjs/toolkit";
 
 
-const intState ={
-    
-}
-const headerTextsSlice = createSlice({
-    name:"headerTexts",
-    initialState:
-})
+// const intState ={
+//     1 :"11111111111111111111",
+//     2 :"22222222222222222222",
+//     3 :"33333333333333333333",
+//     4 :"444444444444444444444"
+// }
+// const headerTextsSlice = createSlice({
+//     name:"headerTexts",
+//     initialState:intState ,
+//     reducers:{
+
+//     }
+
+// })
