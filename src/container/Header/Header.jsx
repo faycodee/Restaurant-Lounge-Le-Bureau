46456f98f673b64c2c3b1dad6cd37daad2feef73
Faycal <faysaloumzil1@gmail.com>
2025-01-13 07:36:38 +0100

    Faycal: reduce time for loeder

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -18,7 +18,7 @@ const Header = () => {
   const nextVidRef = useRef(null);
   const nextIndex = (curIndex % totalVideos) + 1;
   const [fiveSecend, setfiveSecend] = useState(false);
-  setTimeout(() => setfiveSecend(true), 5000);
+  setTimeout(() => setfiveSecend(true), 2000);
   const getSrc = (i) => `./videos/${i}.mp4`;
   useEffect(() => {
     if (loadedVideos == totalVideos - 2 && fiveSecend) {
