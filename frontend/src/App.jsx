680ac9fa3414f87bfed193c335176e60b325c89f
Faycal <faysaloumzil1@gmail.com>
2025-02-08 22:45:38 +0100

    Faycal: add router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { BrowserRouter  , Routes, Route, Link } from 'react-router-dom';
+// import './i18n';
+import './App.css';
+import { Home, Navbar } from './components';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => (
+  <BrowserRouter>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/special-menu" element={<SpecialMenu />} />
+        <Route path="/chef" element={<Chef />} />
+        <Route path="/intro" element={<Intro />} />
+        <Route path="/laurels" element={<Laurels />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/find-us" element={<FindUs />} /> */}
+      </Routes>
+      {/* <Footer /> */}
+    </div>
+  </BrowserRouter>
+);
 
-  return (
-    <>
-
-      <h1 >Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+export default App;
---
+++ frontend/src/components/index.js
@@ -0,0 +1,23 @@
+import Home from './Home';
+import Navbar from './Navbar';
+// import AboutUs from './AboutUs/AboutUs';
+// import Chef from './Chef/Chef';
+// import FindUs from './Findus/FindUs';
+// import Footer from './Footer/Footer';
+// import Gallery from './Gallery/Gallery';
+// import Intro from './Intro/Intro';
+// import Laurels from './Laurels/Laurels';
+// import SpecialMenu from './Menu/SpecialMenu';
+
+export {
+  Home ,
+  Navbar
+  // AboutUs,
+  // Chef,
+  // FindUs,
+  // Footer,
+  // Gallery,
+  // Intro,
+  // Laurels,
+  // SpecialMenu,
+};
