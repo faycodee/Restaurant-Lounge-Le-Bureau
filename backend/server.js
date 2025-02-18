8e2cb40355691f01bbe95d0c9ff1254ce1a1361f
Faycal <faysaloumzil1@gmail.com>
2025-02-18 12:00:14 +0100

    Faycal:edit backend and try to deploy itr

--- backend/server.js
+++ backend/server.js
@@ -8,10 +8,7 @@ const usersRoutes = require("./routes/usersRoutes");
 require("dotenv").config(); // لاستخدام المتغيرات البيئية
 const app = express();
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(process.env.MONGO_URI);
 
 const db = mongoose.connection;
 db.once("open", () => console.log("Connected to MongoDB"));
